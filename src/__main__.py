4c46d138adb232cb5737478c3d901d05d10a4a86
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-06-10 14:53:23 +0300

    Refactoring the code running logic to python_packages.code_runner.

--- src/__main__.py
+++ src/__main__.py
@@ -1,32 +1,25 @@
 """The entry point for the entire package."""
-import sys, os, time, winsound
+import sys, os, winsound
 
 def main():
-    """Starts the script by calling the `begin_script()` function from the `scriptRunner` extension module."""
+    """Starts the python script by calling either the `run_script` or `run_script_with_profiling` function of the code_runner module."""
     
     # Changing the working directory to where this script is.
     os.chdir(os.path.dirname(__file__))
     
     # This line adds the directory path of this module to the sys.path list.
-    # sys.path is a list of strings that specifies the search path for Python modules.
-    # By adding the directory path of the script file to this list, it allows Python to
-    # locate and import any modules in that directory as well as any subdirectories within it.
+    # sys.path is a list of strings that specifies where to search for Python modules.
     sys.path.append(os.path.dirname(__file__))
     
-    from extensions.primeFinder.primeFinder import prime_finder
-    from extensions.optimizedPrimeFinder.optimizedPrimeFinder import optimized_prime_finder
+    if len(sys.argv) > 1 and sys.argv[1] in ("-p", "--profile", "--prof"):
+        from python_packages.code_runner.code_runner import run_script_with_profiling
+        
+        run_script_with_profiling()
     
-    start = time.time()
-    len1 = len(prime_finder(0, 10_000))
-    time1 = time.time() - start
-    print(f"Found primes are: {len1}. Took {time1} seconds.")
-    
-    start = time.time()
-    len2 = len(optimized_prime_finder(0, 10_000))
-    time2 = time.time() - start
-    print(f"Found primes are: {len2}. Took {time2} seconds.")
-    
-    print(f"Optimized version is {time1 / time2} times faster than the original version.")
+    else:
+        from python_packages.code_runner.code_runner import run_script
+        
+        run_script()
 
 if __name__ == '__main__':
     main()
