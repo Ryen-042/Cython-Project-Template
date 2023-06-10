41f579a7837e850e8069ac5e7c8006ee5785a273
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-06-10 14:56:41 +0300

    A simple module to show how to import from the package root.

---
+++ src/prime_finder_configs.py
@@ -0,0 +1,7 @@
+"""This module contains settings for configuring the primeFinder packages."""
+
+MAX_ALLOWED_NUM = 500000
+"""Controls the highest number that can be reached when seaching for primes."""
+
+MIN_ALLOWED_NUM = 2
+"""Controls the lowest number that can be used to start searching for primes from."""
